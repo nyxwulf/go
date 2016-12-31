fde7c17626ff52bea24b0193b179965af2bc5326
Daniel Morsing <daniel.morsing@gmail.com>
2016-12-31 18:39:02 +0000

    fix deps, write barrier
    Change-Id: I2c7dcc0969123ac74e46a068e95f95e597b107e6

--- src/runtime/proc.go
+++ src/runtime/proc.go
@@ -599,6 +599,12 @@ func causalprofStealTime(gp *g) {
 
 // Park goroutine on timer goroutine. Returns if the
 // goroutine needs to sleep. Returns with timers.lock held if true.
+//
+// This function gets called from exitsyscall which initially
+// may not have a P and can not have write barriers
+// However, it only gets called on paths where a P has been
+// acquired
+//go:yeswritebarrierrec
 func causalprofPark(gp *g) bool {
 
 	// causalprofParkTime holds the time at which this
@@ -2693,9 +2699,7 @@ func exitsyscall(dummy int32) {
 			_g_.stackguard0 = _g_.stack.lo + _StackGuard
 		}
 		_g_.throwsplit = false
-		if causalprofPark(_g_) {
-			goparkunlock(&timers.lock, "sleep", traceEvGoSleep, 2)
-		}
+		exitsyscallCausalprof(_g_)
 		return
 	}
 
@@ -2731,6 +2735,15 @@ func exitsyscall(dummy int32) {
 	_g_.syscallsp = 0
 	_g_.m.p.ptr().syscalltick++
 	_g_.throwsplit = false
+}
+
+// this gets called in the P holding section of exitsyscall
+// but goparkunlock has writebarriers
+//go:yeswritebarrierrec
+func exitsyscallCausalprof(gp *g) {
+	if causalprofPark(gp) {
+		goparkunlock(&timers.lock, "sleep", traceEvGoSleep, 2)
+	}
 }
 
 //go:nosplit
@@ -2840,17 +2853,12 @@ func exitsyscall0(gp *g) {
 	}
 	unlock(&sched.lock)
 	if _p_ != nil {
-		// TODO(dmo): figure out if it makes sense to acquire the P here,
-		// if gp needs to be parked for causal profiling.
-		// If the p was previously idle, there aren't enough goroutines to schedule
-		// and running a round of scheduling is wasteful.
 		acquirep(_p_)
-		if causalprofPark(gp) {
-			casgstatus(gp, _Grunnable, _Gwaiting)
-			unlock(&timers.lock)
-			schedule() // Never returns
+		if !causalprofPark(gp) {
+			execute(gp, false) // Never returns.
 		}
-		execute(gp, false) // Never returns.
+		casgstatus(gp, _Grunnable, _Gwaiting)
+		unlock(&timers.lock)
 	}
 	if _g_.m.lockedg != nil {
 		// Wait until another thread schedules gp and so m again.
